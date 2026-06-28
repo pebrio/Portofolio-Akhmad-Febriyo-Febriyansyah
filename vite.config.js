4e39e90139b9534e9d32e124a871d9e934a59881
pebrio <fahirfebrio18@gmail.com>
2026-06-28 13:46:43 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Tambahkan baris ini, sesuaikan dengan nama repository Anda
   base: '/Portofolio-Akhmad-Febriyo-Febriyansyah/',
 })
